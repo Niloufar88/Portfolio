66616a83c447c67432db3c59a4aeac6a3b1d1518
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-07-12 18:21:23 +0330

    adding active class to lang btn

--- scripts/changingLanguage.js
+++ scripts/changingLanguage.js
@@ -1,6 +1,6 @@
 const translations = {
   de: {
-    LogoDevRole: "entwickler",
+    LogoDevRole: "entwicklerin",
     devRole: "Frontend Entwicklerin",
     menuItem1: "wieso ich",
     menuItem2: "Fähigkeiten",
@@ -8,7 +8,7 @@ const translations = {
     menuItem4: "kontaktieren",
     firstSectionTitle: "wieso ich",
     aboutMeText:
-      "Alles beginnt mit einer Idee. Was mich am Programmieren am meisten fasziniert, ist die Fähigkeit, diese Idee in etwas Reales zu verwandeln – etwas, das Menschen sehen, nutzen und erleben können. Für mich bedeutet Codieren, Rätsel zu lösen, Herausforderungen anzunehmen und einen Weg nach vorne zu finden, selbst wenn die Lösung nicht offensichtlich ist. Jeder Bug hat uns etwas beizubringen. Jede Sackgasse eröffnet eine neue Perspektive. Jedes Problem birgt das Potenzial für seine eigene Lösung – man muss nur weiter danach suchen. Am Ende wird großartige Software nicht einfach nur gebaut. Sie wird handgefertigt – Zeile für Zeile.",
+      "Alles beginnt mit einer Idee. <br /> Was mich am Programmieren am meisten fasziniert, ist die Fähigkeit, diese Idee in etwas Reales zu verwandeln – etwas, das Menschen sehen, nutzen und erleben können.<br /> Für mich bedeutet Codieren, Rätsel zu lösen, Herausforderungen anzunehmen und einen Weg nach vorne zu finden, selbst wenn die Lösung nicht offensichtlich ist. <br /> Jeder Bug hat uns etwas beizubringen. Jede Sackgasse eröffnet eine neue Perspektive. Jedes Problem birgt das Potenzial für seine eigene Lösung – man muss nur weiter danach suchen. <br />Am Ende wird großartige Software nicht einfach nur gebaut. Sie wird handgefertigt – Zeile für Zeile.",
   },
   en: {
     LogoDevRole: "developer",
@@ -19,7 +19,7 @@ const translations = {
     menuItem4: "Contact",
     firstSectionTitle: "why Me",
     aboutMeText:
-      "Everything begins with an idea. What fascinates me most about programming is turning that idea into something real—something people can see, use, and experience. To me, coding is about solving puzzles, embracing challenges, and finding a way forward even when the solution isn't obvious. Every bug has something to teach. Every dead end reveals a new perspective. Every problem holds the potential for its own solution—you just have to keep looking. In the end, great software isn't just built. It's crafted—one line of code at a time.",
+      "Everything begins with an idea.<br />  What fascinates me most about programming is turning that idea into something real—something people can see, use, and experience. <br />To me, coding is about solving puzzles, embracing challenges, and finding a way forward even when the solution isn't obvious. <br />Every bug has something to teach. Every dead end reveals a new perspective. Every problem holds the potential for its own solution—you just have to keep looking.<br /> In the end, great software isn't just built. It's crafted—one line of code at a time.",
   },
 };
 
@@ -33,10 +33,17 @@ function changeLanguage(lang) {
     const key = element.getAttribute("data-i18n");
 
     if (translations[lang] && translations[lang][key])
-      element.textContent = translations[lang][key];
+      element.innerHTML = translations[lang][key];
   });
-
+  updateLangIcon(lang);
   localStorage.setItem("prefferdLanguage", lang);
+}
+
+function updateLangIcon(lang) {
+  document.querySelectorAll("#lang-switcher a").forEach((a) => {
+    if (a.getAttribute("data-id") === lang) a.classList.add("active");
+    else a.classList.remove("active");
+  });
 }
 
 languageSwitcher.addEventListener("click", (e) => {
