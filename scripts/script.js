3d5c23f815eddd1c35ded9599ea18151606c0931
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-07-12 19:01:39 +0330

    adding active class to menu btns

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,9 @@ const languageEl = document.querySelectorAll(".header-language-text");
 const heroSection = document.getElementById("hero-section");
 const header = document.querySelector(".header");
 
-// const arrowDownBtn = document.querySelector(".arrow-down");
-// const targetSection = document.getElementById("why-me");
-
-// arrowDownBtn.addEventListener("click", () => {
-//   // 1. Get the current scroll position
-//   const currentScroll = window.scrollY;
-
-//   // 2. Get the distance from the top of the screen to the section
-//   const sectionTopRelative = targetSection.getBoundingClientRect().top;
-
-//   // 3. Add them together to get the absolute position from the very top of the page
-//   const absoluteTargetPosition = currentScroll + sectionTopRelative;
-
-//   // 4. Run your spring animation function
-//   springScroll(absoluteTargetPosition);
-// });
-
-function eclipseBgActive() {
-  languageEl.forEach((langBtn) => {
-    langBtn.classList.add("active");
-  });
-}
-
 // fixing header postion when hero section is scrolled down completely
 window.addEventListener("scroll", function () {
   const scrollHeight = window.pageYOffset;
-  console.log(`scrollHeight:`, window.pageYOffset);
   const heroSectionHeight = heroSection.getBoundingClientRect().height;
 
   if (scrollHeight > heroSectionHeight) {
@@ -38,35 +14,21 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// function springScroll(endPosition) {
-//   const mass = 1;
-//   const stiffness = 100;
-//   const damping = 15;
+//giving active class to menu btns
 
-//   let speed = 0;
+function updateActiveMenuBtn() {
+  const menuLinks = document.querySelectorAll(".header-menu nav ul li");
 
-//   function schritt() {
-//     const distance = endPosition - window.scrollY;
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      menuLinks.forEach((linkItem) => {
+        linkItem.classList.remove("active");
+      });
+      link.classList.add("active");
+    });
+  });
+}
 
-//     // If we are close enough, snap to target and stop
-//     if (Math.abs(distance) < 0.5 && Math.abs(speed) < 0.5) {
-//       window.scrollTo(0, endPosition);
-//       return;
-//     }
+updateActiveMenuBtn(event);
 
-//     // 1. Calculate physics forces
-//     const kraft = distance * stiffness;
-//     const bremsen = speed * damping;
-
-//     // 2. Update speed (multiplied by 0.016 to slow it down per frame)
-//     speed = speed + ((kraft - bremsen) / mass) * 0.016;
-
-//     // 3. Update position (multiplied by 0.016 for smooth rendering)
-//     const neuePosition = window.scrollY + speed * 0.016;
-//     window.scrollTo(0, neuePosition);
-
-//     requestAnimationFrame(schritt);
-//   }
-
-//   requestAnimationFrame(schritt);
-// }
+//scroll down function to
